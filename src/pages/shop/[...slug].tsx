c08cd3b519510321187ed40959c04a6d7c847ede
rizan1998 <rizanalfalah@gmail.com>
2024-03-27 23:50:34 +0700

    learn about multiple param in next router segment learn about how to param not avaible or is null

--- src/pages/shop/[...slug].tsx
+++ src/pages/shop/[...slug].tsx
@@ -1,13 +1,15 @@
 import { useRouter } from "next/router";
 
-const shoPage = () => {
+const shopPage = () => {
   const { query } = useRouter();
   console.log(query);
   return (
     <div>
       <h1>shoping page</h1>
+      <p>{query.slug && query.slug[0] + "-" + query.slug[1]}</p>
+      {/* <p>shop {`${query.slug ? query.slug[0] : "" + "-" + query.slug[0]}`}</p> */}
     </div>
   );
 };
 
-export default shoPage;
+export default shopPage;
